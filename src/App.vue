104a69ca2d9aae61a1dc0f725dc90f3349d16df9
Mulia Nasution <mul14@users.noreply.github.com>
2021-10-27 22:08:45 +0700

    Add base64 encode (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { shallowRef } from 'vue';
 
-const currentComponent = shallowRef();
+import Base64Encode from './views/Base64Encode.vue';
+
+const currentComponent = shallowRef(Base64Encode);
 
 const changeComponent = (name) => {
   currentComponent.value = name
@@ -14,6 +16,10 @@ const changeComponent = (name) => {
       <h1 class="font-bold">DevTools.id</h1>
       <hr class="my-4"/>
       <div class="space-y-4 sticky overflow-auto h-screen">
+        <dl>
+          <dt>Base64</dt>
+          <dl class="ml-4"><a href="javascript:;" @click="changeComponent(Base64Encode)" class="underline">Encode</a></dl>
+        </dl>
       </div>
     </div>
     <div class="w-5/6 px-6 lg:px-10 py-10 bg-gray-50">
---
+++ src/views/Base64Encode.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { encode } from 'js-base64'
+
+const input = ref('Hello World!')
+const output = ref('')
+
+const submit = () => {
+  output.value = encode(input.value)
+}
+</script>
+
+<template>
+  <div class>
+    <h1 class="text-3xl">Base64 Encode</h1>
+    <hr class="my-6" />
+    <div class="flex flex-col gap-4">
+      <div>
+        <textarea
+          v-model="input"
+          class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 font-mono"
+          rows="8"
+        ></textarea>
+        <button
+          @click="submit"
+          class="rounded-lg px-4 py-2 text-white bg-gray-400 hover:bg-gray-500"
+        >Encode</button>
+      </div>
+      <div>
+        <textarea
+          v-model="output"
+          class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 font-mono bg-gray-100"
+          rows="8"
+          readonly
+        ></textarea>
+      </div>
+    </div>
+  </div>
+</template>
