30659e7647f04a866137edaaf8e59a55b3bb0a4c
Mulia Nasution <mul14@users.noreply.github.com>
2021-10-31 13:08:30 +0700

    Add vue router (#5)
    * Add Vue Router
    
    * Update Vue Router version

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,7 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue';
 
-import Base64Encode from './views/Base64Encode.vue';
-import Base64Decode from './views/Base64Decode.vue';
-import UrlEncode from './views/UrlEncode.vue';
-import UrlDecode from './views/UrlDecode.vue';
-
-const currentComponent = shallowRef(Base64Encode);
-
-const changeComponent = (name) => {
-  currentComponent.value = name
-}
 </script>
 
 <template>
-  <div class="flex">
-    <div class="w-1/6 p-4 border-r relative">
-      <h1 class="font-bold">DevTools.id</h1>
-      <hr class="my-4"/>
-      <div class="space-y-4 sticky overflow-auto h-screen">
-        <dl>
-          <dt>Base64</dt>
-          <dl class="ml-4"><a href="javascript:;" @click="changeComponent(Base64Encode)" class="underline">Encode</a></dl>
-          <dl class="ml-4"><a href="javascript:;" @click="changeComponent(Base64Decode)" class="underline">Decode</a></dl>
-        </dl>
-        <dl>
-          <dt>URL</dt>
-          <dl class="ml-4"><a href="javascript:;" @click="changeComponent(UrlEncode)" class="underline">Encode</a></dl>
-          <dl class="ml-4"><a href="javascript:;" @click="changeComponent(UrlDecode)" class="underline">Decode</a></dl>
-        </dl>
-      </div>
-    </div>
-    <div class="w-5/6 px-6 lg:px-10 py-10 bg-gray-50">
-      <component :is="currentComponent"></component>
-    </div>
-  </div>
+  <router-view />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
 import './main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
