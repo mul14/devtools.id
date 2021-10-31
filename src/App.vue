a05e5fdb08f638391cf63a4e903b0435f7fd8db5
Mulia Nasution <mul14.net@gmail.com>
2021-10-31 13:16:15 +0700

    Use lazy loading routes

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,33 @@
 </script>
 
 <template>
-  <router-view />
+  <div class="flex">
+    <div class="w-1/6 p-4 border-r relative">
+      <h1 class="font-bold">DevTools.id</h1>
+      <hr class="my-4" />
+      <div class="space-y-4 sticky overflow-auto h-screen">
+        <dl>
+          <dt>Base64</dt>
+          <dd>
+            <router-link to="/base64-decode">Base64 Decode</router-link>
+          </dd>
+          <dd>
+            <router-link to="/base64-encode">Base64 Encode</router-link>
+          </dd>
+        </dl>
+        <dl>
+          <dt>URL</dt>
+          <dd>
+            <router-link to="/url-decode">URL Decode</router-link>
+          </dd>
+          <dd>
+            <router-link to="/url-encode">URL Encode</router-link>
+          </dd>
+        </dl>
+      </div>
+    </div>
+    <div class="p-6 w-full">
+      <router-view />
+    </div>
+  </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from './views/Home.vue';
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: () => import('./views/Home.vue') },
+  { path: '/base64-decode', component: () => import('./views/Base64Decode.vue') },
+  { path: '/base64-encode', component: () => import('./views/Base64Encode.vue') },
+  { path: '/url-decode', component: () => import('./views/UrlDecode.vue') },
+  { path: '/url-encode', component: () => import('./views/UrlEncode.vue') },
 ]
 
 const router = createRouter({
