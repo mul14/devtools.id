8776a4bd723e5c0e38a8158cce9e8f08075a675d
Mulia Nasution <mul14@users.noreply.github.com>
2021-10-31 13:57:19 +0700

    Add search menu (#8)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+  import { computed, reactive, ref } from 'vue'
 
+  const search = ref('');
+
+  const menus = reactive([
+    { name: 'Base64 Decode', slug: 'base64-decode' },
+    { name: 'Base64 Encode', slug: 'base64-encode' },
+    { name: 'URL Decode', slug: 'url-decode' },
+    { name: 'URL Encode', slug: 'url-encode' },
+    { name: 'Hash Generator', slug: 'hash-generator' },
+  ]);
+
+  const filteredMenus = computed(() => menus.filter((menu) => menu.name.toLowerCase().includes(search.value)))
 </script>
 
 <template>
@@ -8,27 +20,10 @@
       <h1 class="font-bold">DevTools.id</h1>
       <hr class="my-4" />
       <div class="space-y-4 sticky overflow-auto h-screen">
+        <input type="search" v-model="search" placeholder="Type to search" class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 bg-gray-100" />
         <dl>
-          <dt>Base64</dt>
-          <dd>
-            <router-link to="/base64-decode">Base64 Decode</router-link>
-          </dd>
-          <dd>
-            <router-link to="/base64-encode">Base64 Encode</router-link>
-          </dd>
-        </dl>
-        <dl>
-          <dt>URL</dt>
-          <dd>
-            <router-link to="/url-decode">URL Decode</router-link>
-          </dd>
-          <dd>
-            <router-link to="/url-encode">URL Encode</router-link>
-          </dd>
-        </dl>
-        <dl>
-          <dd>
-            <router-link to="/hash-generator">Hash Generator</router-link>
+          <dd v-for="(menu, index) in filteredMenus" :key="index">
+            <router-link :to="menu.slug">{{ menu.name }}</router-link>
           </dd>
         </dl>
       </div>
