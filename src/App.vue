2d1966378b62d5b668149e82bdf920c69441a3b6
Mulia Nasution <mul14@users.noreply.github.com>
2021-11-04 01:54:21 +0700

    Update layout

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,30 @@
 
 <template>
   <div class="flex">
-    <div class="w-1/6 p-4 border-r relative">
-      <h1 class="font-bold">DevTools.id</h1>
+    <div class="w-3/12 p-4 border-r relative">
+      <h1 class="font-bold">
+        <router-link to="/" title="Trivial Tools and Utilities for Developers">
+          DevTools.id
+        </router-link>
+      </h1>
       <hr class="my-4" />
-      <div class="space-y-4 sticky overflow-auto h-screen">
-        <input type="search" v-model="search" placeholder="Type to search" class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 bg-gray-100" />
-        <dl>
+      <input type="search" v-model="search" placeholder="Type to search" class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 bg-gray-100" />
+      <nav class="space-y-4 sticky overflow-auto h-screen py-2">
+        <dl class="space-y-0.5">
           <dd v-for="(menu, index) in filteredMenus" :key="index">
-            <router-link :to="menu.slug">{{ menu.name }}</router-link>
+            <router-link :to="menu.slug" class="block rounded-lg hover:bg-gray-300 px-4 py-2">{{ menu.name }}</router-link>
           </dd>
         </dl>
-      </div>
+      </nav>
     </div>
     <div class="px-6 pt-6 pb-56 w-full">
       <router-view />
     </div>
   </div>
 </template>
+
+<style scoped>
+  nav .router-link-exact-active {
+    background: #e5e7eb;
+  }
+</style>
