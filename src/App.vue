1d2929f0046ca6c82c360f3b79f978d5b93f3f3b
Mulia Nasution <mul14@users.noreply.github.com>
2021-11-05 13:43:50 +0700

    Add next and previous buttons

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <script setup lang="ts">
-  import { computed, ref } from 'vue'
+  import { computed, ref, reactive, watch } from 'vue'
   import { useStore } from './store';
+  import { useRouter, useRoute } from 'vue-router';
+  import Arrow from "./icons/Arrow.vue";
+
+  const router = useRouter();
+  const route = useRoute();
+  const historyState = reactive({
+    allowBack: false,
+    allowForward: false,
+  })
 
   const search = ref('');
 
   const { menus } = useStore();
 
   const filteredMenus = computed(() => menus.filter((menu) => menu.name.toLowerCase().includes(search.value)))
+
+  watch(
+    () => route.fullPath,
+    (val, oldVal) => {
+      historyState.allowBack = window.history.state.back
+      historyState.allowForward = window.history.state.forward
+    }
+  )
 </script>
 
 <template>
   <div class="flex">
     <div class="w-3/12 p-4 border-r relative">
-      <h1 class="font-bold">
-        <router-link to="/" title="Trivial Tools and Utilities for Developers">
-          DevTools.id
-        </router-link>
-      </h1>
+      <div class="flex justify-between items-center">
+        <h1 class="font-bold">
+          <router-link to="/" title="Trivial Tools and Utilities for Developers">
+            DevTools.id
+          </router-link>
+        </h1>
+        <nav>
+          <button @click="router.back()" :disabled="!historyState.allowBack" class="px-2 py-2 rounded-lg border border-transparent text-gray-600 hover:border-gray-300 active:bg-gray-100 disabled:opacity-25 disabled:hover:border-transparent disabled:active:bg-transparent" title="Previous">
+            <Arrow class="rotate-180 w-5" />
+          </button>
+          <button @click="router.forward()" :disabled="!historyState.allowForward" class="px-2 py-2 rounded-lg border border-transparent text-gray-600 hover:border-gray-300 active:bg-gray-100 disabled:opacity-25 disabled:hover:border-transparent disabled:active:bg-transparent" title="Next">
+            <Arrow class="w-5"/>
+          </button>
+        </nav>
+      </div>
       <hr class="my-4" />
       <input type="search" v-model="search" placeholder="Type to search" class="w-full rounded-lg border border-gray-300 focus:outline-none p-2 bg-gray-100" />
       <nav class="space-y-4 sticky overflow-auto h-screen py-2">
---
+++ src/icons/Arrow.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+</template>
