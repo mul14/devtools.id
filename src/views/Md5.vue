32cb2a5a1f8b3ea2a4d1a30d604fe5b3a5d00ab8
Mulia Nasution <mul14@users.noreply.github.com>
2022-10-31 23:27:36 +0700

    Add MD5 Hash

---
+++ src/views/Md5.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import md5 from 'md5'
+
+const input = ref('admin')
+const output = ref('21232f297a57a5a743894a0e4a801fc3')
+
+const submit = () => {
+  output.value = md5(input.value)
+}
+</script>
+
+<template>
+  <div class>
+    <h1 class="font-normal text-2xl text-darkBlue uppercase">MD5 Hash</h1>
+    <div class="border-b-2 border-gray mb-8 mt-2"></div>
+    <div class="flex flex-col gap-4">
+      <div>
+        <textarea
+          v-model="input"
+          class="block w-full shadow-custom rounded-md border-indigo-500 py-3 px-4 focus:border-indigo-500 focus:ring-indigo-500"
+          rows="8"
+        ></textarea>
+        <button
+          @click="submit"
+          class="rounded-md my-4 px-4 py-2 text-white bg-primary font-bold text-sm uppercase hover:bg-primary"
+        >Generate Slug</button>
+      </div>
+      <div>
+        <textarea
+          v-model="output"
+          class="block w-full shadow-custom rounded-md border-indigo-500 py-3 px-4 focus:border-indigo-500 focus:ring-indigo-500 bg-gray-100"
+          rows="8"
+          readonly
+        ></textarea>
+      </div>
+    </div>
+  </div>
+</template>
